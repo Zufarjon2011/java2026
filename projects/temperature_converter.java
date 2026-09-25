e3b061a53f4f02fe3d725cb1c8b8b9abdf18d137
Hey Bro <zufarjonxojiakbarov@gmail.com>
2026-09-25 21:24:48 +0500

    Temperature converter
    fahrenheit to celcius
    celcius to fahrenheit
    
    lesson project, ternary operators

---
+++ projects/temperature_converter.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class temperature_converter {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        //declare variables
+        double temperature;
+        double newTemp;
+        String unit;
+
+        System.out.print("Enter temperature: ");
+        temperature = scanner.nextDouble();
+
+        System.out.print("Choose to which unti convert to (C or F): ");
+        unit = scanner.next().toUpperCase();
+
+        newTemp = (unit.equals("C")) ? (temperature - 32) * 5/9 : (temperature * 9/5) + 32;
+
+        System.out.printf("%.1f°%s", newTemp, unit);
+
+
+
+        scanner.close();
+    }
+}
